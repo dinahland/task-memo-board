9fa784c17e68e33e9ed0c988d687c6b03b1e4e6c
sunyoung <hsunnies@gamil.com>
2025-07-18 23:58:43 +0900

    feat: Validation 추가

--- src/main/java/com/example/board/dto/BoardRequestDto.java
+++ src/main/java/com/example/board/dto/BoardRequestDto.java
@@ -1,12 +1,18 @@
 package com.example.board.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class BoardRequestDto {      /*게시글 생성 요청 DTO*/
+    @NotNull
     private String title;
+
+    @NotNull
     private String contents;
+
+    @NotNull
     private String username;
 }
--- src/main/java/com/example/board/dto/SignUpRequestDto.java
+++ src/main/java/com/example/board/dto/SignUpRequestDto.java
@@ -1,13 +1,23 @@
 package com.example.board.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @AllArgsConstructor
 public class SignUpRequestDto {     /*회원 가입 요청 DTO*/
+
+    @NotNull
     private String username;
+
+    @NotBlank
     private String password;
+
+    @NotNull
+    @Range(min = 1, max = 150)
     private Integer age;
 
 }
