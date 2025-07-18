8a60074cc50be5b2be740a79912a870177610ddd
sunyoung <hsunnies@gamil.com>
2025-07-18 14:22:53 +0900

    feat: Query Methods 추가 (수정 시간 내림차순 정렬)

--- src/main/java/org/example/memo/repository/MemoRepository.java
+++ src/main/java/org/example/memo/repository/MemoRepository.java
@@ -3,7 +3,8 @@ package org.example.memo.repository;
 import org.example.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
